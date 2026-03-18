a8cdedbc99058d93b7d226965edce0ef5b140884
Terry Gilmore <tgilly93@gmail.com>
2026-03-18 19:08:20 -0400

    TG set up Spring Boot Config for DB.

---
+++ backend/jobmatch/src/test/java/jobmatch/JobmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package jobmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
